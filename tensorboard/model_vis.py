fb5f9e76f7d1cb6a6641c41ce5f055c8fc244839
Kiyoon Kim <yoonkr33@gmail.com>
2021-10-14 01:51:45 +0900

    tensorboard code update

--- tensorboard/model_vis.py
+++ tensorboard/model_vis.py
@@ -5,6 +5,7 @@ import torchvision
 model = torchvision.models.resnet50()
 dummy_input = torch.zeros((1, 3, 224, 224))
 
-train_writer = SummaryWriter('exp1/train'), comment='train')
+train_writer = SummaryWriter('exp1/train', comment='train')
 
 train_writer.add_graph(model, dummy_input)
+train_writer.flush()
--- tensorboard/scalar_example.py
+++ tensorboard/scalar_example.py
@@ -1,10 +1,11 @@
-
 from torch.utils.tensorboard import SummaryWriter
 import random
 
-train_writer = SummaryWriter('exp1/train'), comment='train')
-val_writer = SummaryWriter('exp1/val'), comment='val')
+train_writer = SummaryWriter('exp1/train', comment='train')
+val_writer = SummaryWriter('exp1/val', comment='val')
 
 for epoch in range(100):
     loss = random.random()
+    val_loss = random.random()+0.5
     train_writer.add_scalar('Loss', loss, epoch)
+    val_writer.add_scalar('Loss', val_loss, epoch)
